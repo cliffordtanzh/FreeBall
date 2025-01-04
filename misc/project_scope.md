4aa9161d04faa6b8175b0d22a2dc5a1a55153de6
clifford <cliff.tan@profileprint.ai>
2025-01-04 23:57:04 +0800

    4/1/2025

--- misc/project_scope.md
+++ misc/project_scope.md
@@ -1,14 +1,14 @@
 ## FreeBall - Volleyball Analysis Tool
 
 ### Scope of Work
-1. Ball location prediction
-    Inputs: Video Frame (360 x 640), Grayscale or Color not yet decided
+1. Ball location prediction \
+    Inputs: Video Frame (360 x 640), Grayscale or Color not yet decided \
     Outputs: 2D Pixel Coordinates of ball location, estimated ball pixel radius
 
     Might not need machine learning if we are able to use background estimation to remove background and only look for circular-ish objects. Can use centroid tracker to keep tracking the ball while its in the frame.
 
-2. Determining Ball 3D Velocity 
-    Inputs: Temporal Group of Frames (360 x 640 x N)
+2. Determining Ball 3D Velocity \
+    Inputs: Temporal Group of Frames (360 x 640 x N) \
     Outputs: Heatmap showing ball velocity in same dimension as inputs
 
 Considerations
--- src/utils.py
+++ src/utils.py
@@ -2,28 +2,30 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-def plot_image(image, figax = None, bgr = True):
+def plot_image(image, figax = None, mode = "bgr"):
     if figax is None:
         fig = plt.figure(figsize = (8, 6))
         ax = fig.add_subplot(111)
     else:
         fig, ax = figax
 
-    if bgr:
+    if mode == "bgr":
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     
-    ax.imshow(image)
+    ax.imshow(image, cmap = "gray" if mode == "gray" else None)
     ax.axis("off")
 
     return fig, ax
 
 
-def stream_video(frames):
+def stream_video(frames, fps = 30):
     _, _, _, num_frames = frames.shape
     for i in range(num_frames):
         frame = frames[:, :, :, i]
         cv2.imshow("stream", frame)
-        key = cv2.waitKey(1)
+        cv2.setWindowProperty("stream", cv2.WND_PROP_TOPMOST, 1)
+
+        key = cv2.waitKey(fps)
 
         if key == ord("q"):
             break
